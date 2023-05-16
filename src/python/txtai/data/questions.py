5bde6d524aaa34d49346efca454ef6b8a71f7be3
Fatemah Alsubaiei <47798755+SubaieiFatemah@users.noreply.github.com>
2023-05-16 17:23:52 +0300

    Update questions.py

--- src/python/txtai/data/questions.py
+++ src/python/txtai/data/questions.py
@@ -1,3 +1,7 @@
+"""
+Questions module
+"""
+
 from .base import Data
 
 
@@ -12,7 +16,7 @@ class Questions(Data):
 
         Args:
             tokenizer: model tokenizer
-            columns: tuple of columns to use for question/context/answer/answer_start
+            columns: tuple of columns to use for question/context/answer
             maxlength: maximum sequence length
             stride: chunk size for splitting data for QA tasks
         """
@@ -22,14 +26,21 @@ class Questions(Data):
         if not self.columns:
             self.columns = ("question", "context", "answers", "answer_start")
 
-        self.question, self.context, self.answer, self.answer_start = self.columns
+        self.question, self.context, self.answer, self.answer_start = self.columns#assigning
         self.stride = stride
         self.rpad = tokenizer.padding_side == "right"
 
-    # Rest of your code...
-
     def process(self, data):
-        # Rest of your code...
+        # Tokenize data
+        tokenized = self.tokenize(data)
+
+        # Get mapping of overflowing tokens and answer offsets
+        samples = tokenized.pop("overflow_to_sample_mapping")
+        offsets = tokenized.pop("offset_mapping")
+
+        # Start/end positions
+        tokenized["start_positions"] = []
+        tokenized["end_positions"] = []
 
         for x, offset in enumerate(offsets):
             # Label NO ANSWER with CLS token
@@ -40,9 +51,68 @@ class Questions(Data):
             sequences = tokenized.sequence_ids(x)
 
             # Get and format answer
-            answers = self.answers(data, samples[x], index)
+            answers = self.answers(data, samples[x])
 
-            # Rest of your code...
+            # If no answers are given, set cls token as answer.
+            if len(answers["answer_start"]) == 0:
+                tokenized["start_positions"].append(clstoken)
+                tokenized["end_positions"].append(clstoken)
+            else:
+                # Start/end character index of the answer in the text.
+                startchar = answers["answer_start"][0]
+                endchar = startchar + len(answers["text"][0])
+
+                # Start token index of the current span in the text.
+                start = 0
+                while sequences[start] != (1 if self.rpad else 0):
+                    start += 1
+
+                # End token index of the current span in the text.
+                end = len(inputids) - 1
+                while sequences[end] != (1 if self.rpad else 0):
+                    end -= 1
+
+                # Label with CLS token if out of span
+                if not (offset[start][0] <= startchar and offset[end][1] >= endchar):
+                    tokenized["start_positions"].append(clstoken)
+                    tokenized["end_positions"].append(clstoken)
+                else:
+                    # Map start character and end character to matching token index
+                    while start < len(offset) and offset[start][0] <= startchar:
+                        start += 1
+                    tokenized["start_positions"].append(start - 1)
+
+                    while offset[end][1] >= endchar:
+                        end -= 1
+                    tokenized["end_positions"].append(end + 1)
+
+        return tokenized
+
+    def tokenize(self, data):
+        """
+        Tokenizes batch of data
+
+        Args:
+            data: input data batch
+
+        Returns:
+            tokenized data
+        """
+
+        # Trim question whitespace
+        data[self.question] = [x.lstrip() for x in data[self.question]]
+
+        # Tokenize records
+        return self.tokenizer(
+            data[self.question if self.rpad else self.context],
+            data[self.context if self.rpad else self.question],
+            truncation="only_second" if self.rpad else "only_first",
+            max_length=self.maxlength,
+            stride=self.stride,
+            return_overflowing_tokens=True,
+            return_offsets_mapping=True,
+            padding=True,
+        )
 
     def answers(self, data, index):
         """
@@ -66,6 +136,6 @@ class Questions(Data):
             if not answers:
                 answers = {"text": [], "answer_start": []}
             else:
-                answers = {"text": [answers], "answer_start": [int(answer_start)]} # Note the change here
+                answers = {"text": [answers], "answer_start": [int(answer_start)]}#context.index(answers)
 
         return answers
